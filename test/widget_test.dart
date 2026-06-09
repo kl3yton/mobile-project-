d973f853ff96bf2b63e8955cfad6441c6e921860
José Kleyton <106536950+kl3yton@users.noreply.github.com>
2026-06-09 10:25:13 -0300

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:unipas/main.dart';
-
-void main() {
-  testWidgets('App inicia corretamente', (WidgetTester tester) async {
-    await tester.pumpWidget(const UnipasApp());
-  });
-}
